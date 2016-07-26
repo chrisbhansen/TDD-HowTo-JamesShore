ea17353b3995d69ec0510573ed0646cd291f393c
hopefulmule <chris.hansen@modernmi.com>
2016-07-26 12:03:38 -0600

    Added options to jshint

--- jakefile.js
+++ jakefile.js
@@ -1,4 +1,6 @@
 /*** Created by chrishansen on 7/25/16.***/
+/* globals desc: false, task: false, complete: false, fail: false */
+
 (function () {
     "use strict";
 
@@ -29,8 +31,26 @@
 
         jshint.checkFiles({
             files: "Jakefile.js",
-            options: {},
-            globals: {}
+            options: {
+                bitwise: true,
+                eqeqeq: true,
+                forin: true,
+                freeze: true,
+                futurehostile: true,
+                latedef: "nofunc",
+                noarg: true,
+                nocomma: true,
+                nonbsp: true,
+                nonew: true,
+                strict: true,
+                undef: true,
+
+                node: true,
+                browser: true
+            },
+            globals: {
+
+            }
         }, complete, fail);
     }, { async: true });
 
