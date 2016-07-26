4e22fef8d7d61dc8d3479ea77b76beededb4848f
hopefulmule <chris.hansen@modernmi.com>
2016-07-25 20:04:54 -0600

    Using jake for our build

--- build.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by chrishansen on 7/25/16.
- */
-(function () {
-    "use strict";
-
-    console.log("\n\nBUILD OK");
-
-}());
---
+++ jakefile.js
@@ -0,0 +1,12 @@
+/**
+ * Created by chrishansen on 7/25/16.
+ */
+(function () {
+    "use strict";
+
+    desc("Default build");
+    task("default", function() {
+        console.log("\n\nBUILD OK");
+    });
+
+}());
