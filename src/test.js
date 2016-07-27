756fd0f4c1b809f993afa67c86af5dc436b8835c
hopefulmule <chris.hansen@modernmi.com>
2016-07-27 08:57:47 -0500

    Added floating point test

--- src/test.js
+++ src/test.js
@@ -4,10 +4,16 @@
 
     var assert = require("chai").assert;
 
+
+    // basic addition
     assert.equal(add(3,4), 7);
+
+    // IEEE 754 floating point
+    assert.equal(add(0.1, 0.2), 0.30000000000000004);
+
 
     function add(a,b) {
         return a + b;
-    };
+    }
 
 }());
