f18c786d24182baad23f8799da7ffdf5776d94bc
hopefulmule <chris.hansen@modernmi.com>
2016-07-29 09:26:16 -0500

    Created simpe demonstration app

---
+++ src/app.js
@@ -0,0 +1,10 @@
+/*** Created by chrishansen on 7/29/16.***/
+(function() {
+    "use strict";
+
+    var addition = require("./addition.js");
+
+    console.log("Hello!");
+    console.log("42 + 13 = " + addition.add(42,13));
+
+}());
