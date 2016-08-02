c78aef28e67ffd3c767004d8618a2938a963fd65
hopefulmule <chris.hansen@modernmi.com>
2016-08-02 12:18:00 -0600

    Isolated chai behind assert.js wrapper

---
+++ src/javascript/assert.js
@@ -0,0 +1,11 @@
+/**
+ * Created by chrishansen on 8/2/16.
+ */
+(function() {
+    "use strict";
+
+    var assert = require("../vendor/chai-3.5.0.js").assert;
+
+    module.exports = assert;
+
+}());
